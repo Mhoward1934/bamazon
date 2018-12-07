49cdb928292136feec3ce155c80d41c5be959ed2
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2018-12-07 16:42:09 -0600

    changed DB and added prompts

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,44 +5,67 @@ var inquirer = require("inquirer");
 var Table = require("cli-table");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "MHcookie1969",
-    database: "bamazon_bd"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    connection.end();
-  });
+  host: "localhost",
 
-  
-  function itemForSale() {
-    connection.query("SELECT * FROM products", function (err, result) {
-      var table = new Table({
-        head: ['Item ID', 'Product Name', 'Price', 'Quantity']
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "MHcookie1969",
+  database: "bamazon_DB"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  //connection.end();
+});
+
+function itemForSale() {
+  connection.query("SELECT * FROM products", function (err, result) {
+    console.log("Welcome to Bamazon!");
+    var table = new Table({
+      head: ['Item ID', 'Product Name', 'Price', 'Quantity']
       , colWidths: [10, 30, 10, 10]
     });
-     
-    // table is an Array, so you can `push`, `unshift`, `splice` and friends
-    for(var i = 0; i < result.length; i++){
-      table.push(
-          [result[i].item_id, result[i].product_name, result[i].price, result[i].stock_quantity]
-      );
 
+    for (var i = 0; i < result.length; i++) {
+      table.push(
+        [result[i].item_id, result[i].product_name, result[i].price, result[i].stock_quantity]
+      );
     }
-     
+
     console.log(table.toString());
 
-    })
-    
-  };
-  itemForSale();
+  })
+
+};
+itemForSale();
+
+// function to handle using ordering items from the list.
+function orderItem() {
+  // prompt for item id and quantity.
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "Please enter the Item ID for the product you would like to purchase.",
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like?",
+        validate: function (value) {
+          if (value < stock_quantity) {
+            placeOrder();
+          }
+          console.log("Insufficient quantity!");
+        }
+      },
+    ])
+  }
+  orderItem();
--- bamazon_DB.sql
+++ bamazon_DB.sql
@@ -1,43 +1,43 @@
-DROP DATABASE bamazon_bd;
-CREATE DATABASE bamazon_bd;
-USE bamazon_bd;
+DROP DATABASE bamazon_DB;
+CREATE DATABASE bamazon_DB;
+USE bamazon_DB;
 
 CREATE TABLE products(
 	item_id INTEGER(11) NOT NULL,
     product_name VARCHAR(30) NOT NULL,
     department_name VARCHAR(30) NOT NULL,
-    price DECIMAL(6,2),
+    price INTEGER(11) NOT NULL,
     stock_quantity INTEGER(11) NOT NULL
 );
 
 SELECT * FROM products;
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (1000, "Citizen Watch", "Jewelry", 45.00, 10);
+VALUES (1000, "Citizen Watch", "Jewelry", 45, 10);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (1100, "1ct Diamond Ring", "Jewelry", 1200.00, 5);
+VALUES (1100, "1ct Diamond Ring", "Jewelry", 1200, 5);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (1200, "18 inch Gold Necklace", "Jewelry", 100.00, 10);
+VALUES (1200, "18 inch Gold Necklace", "Jewelry", 100, 10);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (1300, "Bikes", "Toys", 85.00, 15);
+VALUES (1300, "Bikes", "Toys", 85, 15);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (1400, "Playstation", "Toys", 300.00, 20);
+VALUES (1400, "Playstation", "Toys", 300, 20);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (1500, "Monopoly", "Toys", 25.00, 10);
+VALUES (1500, "Monopoly", "Toys", 25, 10);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (1600, "Lawn Mower", "Home and Garden", 250.00, 10);
+VALUES (1600, "Lawn Mower", "Home and Garden", 250, 10);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (1700, "Lawn Edger", "Home and Garden", 75.00, 5);
+VALUES (1700, "Lawn Edger", "Home and Garden", 75, 5);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (1800, "Patio Set", "Home and Garden", 250.00, 5);
+VALUES (1800, "Patio Set", "Home and Garden", 250, 5);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES (1900, "Euro Planter", "Home and Garden", 20.00, 10);
+VALUES (1900, "Euro Planter", "Home and Garden", 20, 10);
